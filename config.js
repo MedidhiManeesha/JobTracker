dfaac0e2b5c5b090c154abb2c9831f44c613f6ca
MedidhiManeesha <medidhimaneesha@gmail.com>
2024-10-12 21:27:12 +0530

    updated

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIsHDpilcYNp7n8YpXuQiE1S8vdNa_I4A",  // Copy from .env
+    authDomain: "jobtracker-caa9a.firebaseapp.com",
+    databaseURL: "https://jobtracker-caa9a-default-rtdb.firebaseio.com",
+    projectId:"jobtracker-caa9a",
+    storageBucket: "jobtracker-caa9a.appspot.com",
+    messagingSenderId: "52907251281",
+    appId: "1:52907251281:web:f090cd0ac8ebe8a54a0a70"
+};
+
+export default firebaseConfig;
--- firebase.js
+++ firebase.js
@@ -1,17 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
 import { getDatabase, ref, set, onValue, get } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js';
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
-
-
+import firebaseConfig from './config.js'; // Import your firebaseConfig from config.js
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -39,5 +28,6 @@ export function saveJobToFirebase(job) {
             console.error("Error saving job: ", error);
         });
 }
-export { get, getDatabase, ref,set };
+
+export { get, getDatabase, ref, set };
 
